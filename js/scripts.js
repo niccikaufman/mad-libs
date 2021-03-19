cf6c3a2436e6c4823e29d10b8922763b5b1d03ad
Nicci Kaufman <niccikaufman@gmail.com>
2021-03-19 12:23:07 -0700

    fix preventDefault event parameter

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-  $("formOne").submit(function(event){
+  event.preventDefault();
+  $("#formOne").submit(function(event){
     const person1Input = $("input#person1").val();
     const person2Input = $("input#person2").val();
     const animalInput = $("input#animal").val();
@@ -15,7 +16,5 @@ $(document).ready(function(){
     $(".noun").text(nounInput);
 
     $("#story").show();
-
-    event.preventDefault();
   });
 });
